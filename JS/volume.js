7af75f10de8f04ad91f1764d63dc0d7b1b044859
Jaacii <sliphno@hotmail.de>
2018-11-26 23:32:04 +0100

    test

--- JS/volume.js
+++ JS/volume.js
@@ -4,10 +4,9 @@
 
   var  sliders = document.getElementsByClassName("slider");
   var  filter = audioContext.createBiquadFilter();
-  var distortion = audioContext.createWaveShaper();
-	distortion.connect(distortion);
+
 	audioElementSource2.connect(filter);
-	distortion.connect(audioContext.destination);
+	filter.connect(audioContext.destination);
 
 for (var i = 0; i < sliders.length; i++) {
     sliders[i].addEventListener("mousemove", sliderchange);
@@ -40,20 +39,8 @@ function sliderchange() {
 	
 	
 
-distortion.curve = makeDistortionCurve(6);
-distortion.oversample = "4x";
 
-function makeDistortionCurve(amount) {    
-    var n_samples = 44100,
-        curve = new Float32Array(n_samples);
-    
-    for (var i = 0; i < n_samples; ++i ) {
-        var x = i * 2 / n_samples - 1;
-        curve[i] = (Math.PI + amount) * x / (Math.PI + (amount * Math.abs(x)));
-    }
-    
-    return curve;
-};
+
 	
 	
 	
