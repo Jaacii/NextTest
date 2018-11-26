460b52ad61366f328a8aeb4cf1a09320743c0314
Jaacii <sliphno@hotmail.de>
2018-11-26 17:42:09 +0100

    test

--- JS/play.js
+++ JS/play.js
@@ -10,7 +10,7 @@
 					audioElement2.src = songs[select];
 				
 					audioElement2.play();
-					btn.style.backgroundImage = 'url(Pictures/pause.png)';
+					btn.style.backgroundImage = 'url(../Pictures/pause.png)';
 					//audioElement2.volume = volumeslider.value / 100;
 					//Pic change
 					//document.getElementById("playpic").src="pause.png";
@@ -19,7 +19,7 @@
 				else {
 					
 					audioElement2.pause(); 
-					btn.style.backgroundImage = 'url(Pictures/play.png)';
+					btn.style.backgroundImage = 'url(../Pictures/play.png)';
 					//audioElement2.volume = 0;		//schummeltaktik, weil es manchmal die audio nicht pausiert
 					//Pic change
 					//document.getElementById("playpic").src="play.png";
