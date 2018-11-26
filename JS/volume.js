abb47f46d95734a9ab0f5f32938fd8d47a268f56
Jaacii <sliphno@hotmail.de>
2018-11-27 00:21:02 +0100

    test

--- JS/volume.js
+++ JS/volume.js
@@ -1,17 +1,55 @@
-//var volumeslider;
-var sliders = document.getElementsByClassName("slider"):
+
+
+var sliders = document.getElementsByClassName("slider"),
+    filter = audioContext.createBiquadFilter();
+    
+
+audioElementSource2.connect(filter);
+filter.connect(audioContext.destination);
+
+for (var i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener("mousemove", changeParameter);
+}
+
+
+function changeParameter() {
+    switch(this.id) {
+        case "volumeslider":
+            audioElement2.volume = this.value / 100;
+            document.getElementById("volumeOutput").innerHTML = this.value + " dB";
+            break;
+        case "gainslider":
+             filter.gain.value = this.value;
+            document.getElementById("gainOutput").innerHTML = this.value + " dB";
+            break;
+        case "qualityslider":
+            filter.Q.value = this.value;
+            document.getElementById("qualityOutput").innerHTML = this.value;
+            break;
+        
+    }
+}
+
+
+
+
+
+
+
+
+/*var sliders = document.getElementsByClassName("slider"):
 var filter = audioContext.createBiquadFilter();
     
 audioElementSource2.connect(filter);
 filter.connect(audioContext.destination);
 
-//Eventlistener adden
+
 for (var i = 0; i < sliders.length; i++) {
     sliders[i].addEventListener("mousemove", sliderchange());
 }
 
 
-//Slider Effekte
+
 function sliderchange() {
     switch(this.id) {
         case "volumeslider":
@@ -30,7 +68,7 @@ function sliderchange() {
     }
 }
 	
-	
+	*/
 	
 	
 	
