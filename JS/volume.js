7e785a3714c02be052b9e615bb717804d62b6920
Jaacii <sliphno@hotmail.de>
2018-11-26 23:42:08 +0100

    test

--- JS/volume.js
+++ JS/volume.js
@@ -1,7 +1,7 @@
 //var volumeslider;
 	
 	
-
+/*
   var  sliders = document.getElementsByClassName("slider");
   var  filter = audioContext.createBiquadFilter();
 
@@ -35,36 +35,12 @@ function sliderchange() {
 }
 	
 	
-	
+	*/
 
 
 
 	
-	
-	
-	//distortion 
-/*	var distortion = context.createWaveShaper();
-audioElementSource2.connect(distortion);
-distortion.connect(context.destination);
 
-distortion.curve = makeDistortionCurve(0);
-distortion.oversample = "4x";
-	
-function makeDistortionCurve(amount) {    
-    var n_samples = 44100,
-        curve = new Float32Array(n_samples);
-    
-    for (var i = 0; i < n_samples; ++i ) {
-        var x = i * 2 / n_samples - 1;
-        curve[i] = (Math.PI + amount) * x / (Math.PI + (amount * Math.abs(x)));
-    }
-    
-    return curve;
-};*/
-	
-	
-	
-	/*
 	
 	var sliders = document.getElementsByClassName("slider");
 	filter = audioContext.createBiquadFilter();
@@ -75,12 +51,7 @@ function makeDistortionCurve(amount) {
     sliders[i].addEventListener("mousemove", sliderchange());
 }
 	
-	var distortion = audioContext.createWaveShaper();
-	source.connect(distortion);
-	distortion.connect(audioContext.destination);
-
-	distortion.curve = makeDistortionCurve(0);
-	distortion.oversample = "4x";
+	
 
 	function sliderchange (id) {
 		
@@ -98,31 +69,15 @@ function makeDistortionCurve(amount) {
 			filter.Q.value = this.value;
 			 document.getElementById("qualityOutput").innerHTML = this.value;
 			break;
-			case "distortionslider":
-			document.getElementById("distortionOutput").innerHTML = this.value;
-			break;
+			
 			
 		}
 	}
-	*/
+	
 		//volumeslider = document.getElementById("volumeslider");		//reagiert plötzlich nicht ?
 		//volumeslider.addEventListener("mousemove", setvolume);
 		
 
-	/*
-
-function makeDistortionCurve(amount) {    
-    var n_samples = 44100,
-        curve = new Float32Array(n_samples);
-    
-    for (var i = 0; i < n_samples; ++i ) {
-        var x = i * 2 / n_samples - 1;
-        curve[i] = (Math.PI + amount) * x / (Math.PI + (amount * Math.abs(x)));
-    }
-    
-    return curve;
-};
-*/
 	
 		
 		
