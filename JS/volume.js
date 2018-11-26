b1db8d9208d243862dd1d81797a246b1c792d7ac
Jaacii <sliphno@hotmail.de>
2018-11-26 23:55:24 +0100

    test

--- JS/volume.js
+++ JS/volume.js
@@ -1,21 +1,18 @@
 //var volumeslider;
-	
-	
-	
-var sliders = document.getElementsByClassName("slider"),
-    filter = audioContext.createBiquadFilter();
+var sliders = document.getElementsByClassName("slider"):
+var filter = audioContext.createBiquadFilter();
     
-
 audioElementSource2.connect(filter);
 filter.connect(audioContext.destination);
 
+//Eventlistener adden
 for (var i = 0; i < sliders.length; i++) {
-    sliders[i].addEventListener("mousemove", changeParameter);
+    sliders[i].addEventListener("mousemove", sliderchange);
 }
 
 
-
-function changeParameter() {
+//Slider Effekte
+function sliderchange() {
     switch(this.id) {
         case "volumeslider":
             audioElement2.volume = volumeslider.value / 100;
@@ -23,7 +20,7 @@ function changeParameter() {
             break;
         case "gainslider":
             filter.gain.value = this.value;
-            document.getElementById("gainOutput").innerHTML = this.value + " dB";
+            document.getElementById("gainOutput").innerHTML = this.value + " dB";		//Evtl mit einem anderen Slider ersetzen?
             break;
         case "qualityslider":
             filter.Q.value = this.value;
@@ -43,53 +40,7 @@ function changeParameter() {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-/*
-  var  sliders = document.getElementsByClassName("slider");
-  var  filter = audioContext.createBiquadFilter();
 
-	
-	audioElementSource2.connect(filter);
-	filter.connect(audioContext.destination);
-
-for (var i = 0; i < sliders.length; i++) {
-    sliders[i].addEventListener("mousemove", sliderchange);
-}
-
-
-
-function sliderchange() {
-    switch(this.id) {
-        case "volumeslider":
-            filter.frequency.value = this.value;
-            document.getElementById("volumeOutput").innerHTML = this.value + " dB";
-            break;
-		case "gainslider":
-            filter.gain.value = this.value;
-            document.getElementById("gainOutput").innerHTML = this.value + " dB";
-            break;
-        case "qualityslider":
-            filter.Q.value = this.value;
-            document.getElementById("qualityOutput").innerHTML = this.value;
-            break;
-       
-        
-    }
-}
-	
-	
-	*/
-
-
-
-	
 
 	
 
