86e6a91126c2a23ab6c8064415cb517f79849fbb
Jaacii <sliphno@hotmail.de>
2018-12-02 17:32:05 +0100

    test

--- JS/moveAudioSource.js
+++ JS/moveAudioSource.js
@@ -2,33 +2,36 @@
 		 var Ycounter =1;
 		  var Xcounter =1;
 		  var Zcounter =1;
+		  
+		  var x=0, y=0, z =1;
+		  
 		 function move(direction) {
 		
 			if (direction === 0) {
 				 y = y + 5 * Ycounter; 
-				audioElement2.setY(y);
+				audioElementSource2.setPosition(x,y,z);
 				Ycounter = Ycounter+1;
 			}
 			else if (direction === 4) {
 				 y = y - 5 * Ycounter; 
-				audioElement2.setY(y);
+				audioElementSource2.setPosition(x,y,z);
 				Ycounter = Ycounter-1;
 			}
 			else if (direction === 1) {
 				 x = x - 5 * Xcounter; 
-				audioElement2.setY(y);
+				audioElementSource2.setPosition(x,y,z);
 				Xcounter = Xcounter-1;
 			}
 			else if (direction === 3) {
 				 x = x + 5 * Ycounter; 
-				audioElement2.setY(y);
+				audioElementSource2.setPosition(x,y,z);
 				Ycounter = Ycounter-1;
 			}
 			else if direction === 2) {
 			Ycounter =1;
 			Xcounter =1;
 			Zcounter =1; 
-			audioElement2.setPosition(0,0,0);
+			audioElementSource2.setPosition(0,0,0);
 			audioElement2.play();
 			}
 			
