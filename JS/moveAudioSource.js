e0e3448ffca074f5a4a25c34ca5894ddd6632fbc
Jaacii <sliphno@hotmail.de>
2018-12-03 15:59:13 +0100

    test

--- JS/moveAudioSource.js
+++ JS/moveAudioSource.js
@@ -7,13 +7,13 @@
 			y = y+1;
 			
 			document.getElementById("Ycoordinate").textContent= y;
-			
+			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 1) {
 			x = x-1;
 			
 			document.getElementById("Xcoordinate").textContent=  x;
-			
+			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 2) {
 			x = 0;
@@ -21,13 +21,13 @@
 			
 			document.getElementById("Xcoordinate").textContent= x;
 			document.getElementById("Ycoordinate").textContent=  y;
-			
+			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 3) {
 			x = x+1;
 			
 			document.getElementById("Xcoordinate").textContent= x;
-			
+			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 4) {
 			
