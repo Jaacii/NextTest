4988ef82e5129c357677c3e22294f0b68b6c7e0f
Jaacii <sliphno@hotmail.de>
2018-12-03 16:42:37 +0100

    test

--- JS/moveAudioSource.js
+++ JS/moveAudioSource.js
@@ -10,14 +10,14 @@
 			y = y+0.1;
 			yDisplay = yDisplay+1;
 			
-			document.getElementById("Ycoordinate").textContent= yDisplay;
+			document.getElementById("Ycoordinate").textContent= y;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 1) {
 			x = x+0.1;
 			xDisplay = x+1;	//Die X-Achse ist gespiegelt. Darum - statt +
 			
-			document.getElementById("Xcoordinate").textContent=  xDisplay ;
+			document.getElementById("Xcoordinate").textContent=  x ;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 2) {
@@ -26,22 +26,22 @@
 			xDisplay = 0; 
 			yDisplay =0;
 			
-			document.getElementById("Xcoordinate").textContent= xDisplay;
-			document.getElementById("Ycoordinate").textContent=  yDisplay;
+			document.getElementById("Xcoordinate").textContent= x;
+			document.getElementById("Ycoordinate").textContent=  y;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 3) {
 			x = x-0.1;
 			xDisplay + xDisplay - 1;
 			
-			document.getElementById("Xcoordinate").textContent= xDisplay ; 
+			document.getElementById("Xcoordinate").textContent= x ; 
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 4) {
 			
 			y = y-0.1;
 			yDisplay = yDisplay-1;
-			document.getElementById("Ycoordinate").textContent= yDisplay;
+			document.getElementById("Ycoordinate").textContent= y;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			
 			}
