d8b34c871f97d90394e852a8d3ca851a420351e9
Jaacii <sliphno@hotmail.de>
2018-12-03 16:52:17 +0100

    test

--- JS/moveAudioSource.js
+++ JS/moveAudioSource.js
@@ -6,14 +6,14 @@
 		 if (direction === 0) {
 			
 			y = y+0.1;
-			y.toFixed(2);
+			y = Math.round (y*100) / 100 
 			
 			document.getElementById("Ycoordinate").textContent= y ;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 1) {
 			x = x+0.1;
-			x.toFixed(2);	//Die X-Achse ist gespiegelt. Darum - statt +
+			x = Math.round (x*100) / 100 	//Die X-Achse ist gespiegelt. Darum - statt +
 			
 			document.getElementById("Xcoordinate").textContent=  ((x*-1)*10)/10 ;
 			resonanceAudioScene.setListenerPosition(x, y, z);
@@ -29,7 +29,7 @@
 			}
 		else if (direction === 3) {
 			x = x-0.1;
-			x.toFixed(2);
+			x = Math.round (x*100) / 100 
 			
 			document.getElementById("Xcoordinate").textContent= ((x*-1)*10)/10  ; 
 			resonanceAudioScene.setListenerPosition(x, y, z);
@@ -37,7 +37,7 @@
 		else if (direction === 4) {
 			
 			y = y-0.1;
-			y.toFixed(2)
+			y = Math.round (y*100) / 100 
 			document.getElementById("Ycoordinate").textContent= y;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			
